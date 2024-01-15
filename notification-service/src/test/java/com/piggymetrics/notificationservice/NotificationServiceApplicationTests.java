80c081be4ba292dbe23df550c0c0e5a3e97bd538
AakashNakarmi <aakasnakarmee@gmail.com>
2024-01-15 22:39:23 +0530

    Initial Commit and Created Config Server and Service Registry

---
+++ notification-service/src/test/java/com/piggymetrics/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piggymetrics.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
